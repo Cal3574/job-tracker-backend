d5d2bcfef663d6deeda886b92150fc0f19d2c081
Callum Work <callumwork@Callums-MacBook-Air.local>
2024-10-14 20:40:58 +0100

    save

--- cmd/main.go
+++ cmd/main.go
@@ -14,13 +14,13 @@ import (
 
 func main() {
 	// Initialize the database connection
+	log.Println("Initializing database connection...")
 	utils.InitDB()
 
-	// Start the CRON jobs
-	// cron.StartCronJobs()
-
 	// Set up routes
+	log.Println("Setting up routes...")
 	router := routes.SetupRoutes()
+	log.Println("Routes set up successfully.")
 
 	// Fetch allowed origins from environment variable
 	allowedOrigins := []string{
@@ -30,7 +30,7 @@ func main() {
 
 	// Check if FRONTEND_URL environment variable is set
 	if frontendURL := os.Getenv("FRONTEND_URL"); frontendURL != "" {
-		log.Printf("Adding allowed origin from environment: %s\n", frontendURL) // Add logging to verify env variable
+		log.Printf("Adding allowed origin from environment: %s\n", frontendURL)
 		allowedOrigins = append(allowedOrigins, frontendURL)
 	} else {
 		log.Println("FRONTEND_URL not set, using defaults.")
@@ -40,6 +40,7 @@ func main() {
 	log.Printf("Allowed Origins: %v\n", allowedOrigins)
 
 	// Start the server with CORS middleware enabled
+	log.Println("Starting server on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(
 		handlers.AllowedOrigins(allowedOrigins),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
