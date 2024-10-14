2e0d92ce30f01fb09bb2bdd6ea9be3a47bda8d10
Callum Work <callumwork@Callums-MacBook-Air.local>
2024-10-14 19:57:57 +0100

    updated cors middleware

--- internal/middleware/cors_middleware.go
+++ internal/middleware/cors_middleware.go
@@ -1,16 +1,21 @@
-// middleware/cors.go
 package middleware
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 )
 
 // CORSHandler adds CORS headers to the response
 func CORSHandler(next http.Handler) http.Handler {
+	allowedOrigin := os.Getenv("FRONTEND_URL")
+	if allowedOrigin == "" {
+		allowedOrigin = "http://localhost:3000" // Fallback if env var is not set
+	}
+
 	return handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://localhost:3000"}), // Replace with your frontend URL
+		handlers.AllowedOrigins([]string{allowedOrigin}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
 		handlers.AllowedHeaders([]string{"Authorization", "Content-Type"}),
 	)(next)
